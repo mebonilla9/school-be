d25532715846fca0d2a2960bc1678b8f5d7a87dc
Manuel Ernesto Bonilla Muñoz <dev_manuel@192.168.1.100>
2021-02-26 18:56:12 -0500

    First commit spring boot project structure

---
+++ src/main/java/co/edu/umb/ds/schoolbe/SchoolbeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umb.ds.schoolbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolbeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SchoolbeApplication.class, args);
+  }
+
+}

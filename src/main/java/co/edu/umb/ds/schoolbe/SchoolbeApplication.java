b40dbcca8fbfb86cc2dbef198a6e6382cef2d3eb
Manuel Ernesto Bonilla Muñoz <dev_manuel@192.168.1.100>
2021-03-05 00:03:48 -0500

    Define cors policy

--- src/main/java/co/edu/umb/ds/schoolbe/SchoolbeApplication.java
+++ src/main/java/co/edu/umb/ds/schoolbe/SchoolbeApplication.java
@@ -2,12 +2,30 @@ package co.edu.umb.ds.schoolbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SchoolbeApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SchoolbeApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+          .allowedOrigins("*")
+          .allowedHeaders("*")
+          .allowedMethods("*")
+          .exposedHeaders("*")
+          .allowedOriginPatterns("*");
+      }
+    };
   }
 
 }
